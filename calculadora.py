5d30845a6c1fa9af4c2364eed1c3706cfecd1b57
RobertoGuaico <changeme@example.com>
2025-03-28 15:30:50 +0000

    bien

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+print ("bienvenido a mi calculadora")
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
+
+
+print(f"la suma de los 2 valores es {num1 + num2}")
